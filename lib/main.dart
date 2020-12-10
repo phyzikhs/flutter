d5d6becc0d72e64e4ea7f1c026abb7e2e529a1e7
Sicelamandla Zikhali <ngelemarweapons@gmail.com>
2020-12-10 14:32:18 +0200

    flt-8 autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('My first app'),
-      centerTitle: true,
-    ),
-    body: Center(
-      child: Text('Hello everyone'),
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Text('Click'),
-    ),
-  ),
+  home: Home(),
 ));
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My first app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Text(
+          'Hello everyone',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.grey[600],
+            fontFamily: 'NerkoOne',
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('Click'),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
