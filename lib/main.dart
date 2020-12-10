b0213bc7fb9732fd7890d7ca387931e9e84cd4eb
Sicelamandla Zikhali <ngelemarweapons@gmail.com>
2020-12-10 14:59:23 +0200

    car icon

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,11 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Image.asset('assets/moon-1.jpg'),
+        child: Icon(
+          Icons.airport_shuttle,
+          color: Colors.lightBlue,
+          size: 50.0,
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
